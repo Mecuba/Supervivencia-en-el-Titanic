104a29dd98de1a215ac2a4cfd33c6fcaf6788b7f
Chiquitico <jjimenezpena7@gmail.com>
2021-12-20 19:59:54 -0600

    a

--- main.py
+++ main.py
@@ -25,12 +25,14 @@ def index():
 
 @app.route('/llegada_datos', methods=['POST'])
 def llegada_datos():
+
     #Llegada de datos
     valores = request.form.getlist('data[]')
+    #Guarda ls valores en un diccionario: 
     guardar_variables_diccionario(valores)
 
-    print(f'data_storage: {data_storage.items()}')
-
+    save_data_juegos()
+    
     return Response(status=200)
 
 
@@ -52,6 +54,27 @@ def guardar_variables_diccionario(valores):
                         data_storage[key][x] = '1'
                     else: 
                         data_storage[key][x] = '0'
+
+def save_data_juegos(): #Datos para enviar :
+    if (data_storage['edad'] < 16): 
+        is_minor = '1' 
+    else: 
+        is_minor = '0'
+
+    datos_juegos = {
+        'Age': [data_storage['edad']],
+        'TravelAlone': [data_storage['viaja_solo']],
+        'Pclass_1': [data_storage['pasajero'][0]],
+        'Pclass_2': [data_storage['pasajero'][1]],
+        'Pclass_3': [data_storage['pasajero'][2]],
+        'Embarked_C': [data_storage['puerto'][0]],
+        'Embarked_Q': [data_storage['puerto'][1]],
+        'Embarked_S': [data_storage['puerto'][2]],
+        'Sex_male': [data_storage['sexo']],
+        'IsMinor': [is_minor],
+        'PassengerId': [1001]
+    }
+    print(f'Datos de juegos: {datos_juegos.items()}')
 
 if __name__ == '__main__': 
     app.run(debug = True, host = HOST)
